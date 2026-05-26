418ebc3599a03f6602ffe1eda6de1e0a57c6af83
Dim <disr9197@gmail.com>
2026-05-26 16:35:09 +0500

    avtomat

--- task.py
+++ task.py
@@ -1,15 +1,22 @@
 #  «Дай задачу на Python уровня Junior+ / Middle на списки и словари»
 
-enter = [[1, 3], [2, 6], [8, 10], [15, 18]]
 
-def merge_intervals(intervals):
-    intervals.sort(key=lambda x: x[0])
-    merged = []
-    for interval in intervals:
-        if not merged or merged[-1][1] < interval[0]:
-            merged.append(interval)
-        else:
-             merged[-1][1] = max(merged[-1][1], interval[1])
-            
-    return merged
-print(merge_intervals(enter))
+menu = {
+    "cola" : 50,
+    "chips" : 60,
+    "chocolate" : 40
+}
+
+print("Добро пожаловать в магазин! Выберите товар:")
+for product, price in menu.items():
+    print(f"- {product}: {price} руб.")
+user_product = input("\nВведите название товара: ").lower().strip() 
+user_money = int(input("Введите количество денег: "))
+if user_product in menu: 
+     if user_money >= menu[user_product]:
+        change = user_money - menu[user_product]
+        print(f"Вы купили {user_product} за {menu[user_product]} руб. Ваша сдача: {change} руб.")
+     else:
+        print("Недостаточно денег")
+else:
+    print("Товар не найден")
