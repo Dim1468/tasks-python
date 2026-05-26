d50f8ffd148d72bf55c2031a6ca11a6336f44981
Dim <disr9197@gmail.com>
2026-05-26 17:05:10 +0500

    Фильтр списка

--- task.py
+++ task.py
@@ -1,22 +1,9 @@
 #  «Дай задачу на Python уровня Junior+ / Middle на списки и словари»
 
 
-menu = {
-    "cola" : 50,
-    "chips" : 60,
-    "chocolate" : 40
-}
-
-print("Добро пожаловать в магазин! Выберите товар:")
-for product, price in menu.items():
-    print(f"- {product}: {price} руб.")
-user_product = input("\nВведите название товара: ").lower().strip() 
-user_money = int(input("Введите количество денег: "))
-if user_product in menu: 
-     if user_money >= menu[user_product]:
-        change = user_money - menu[user_product]
-        print(f"Вы купили {user_product} за {menu[user_product]} руб. Ваша сдача: {change} руб.")
-     else:
-        print("Недостаточно денег")
-else:
-    print("Товар не найден")
+things = ["палатка", "нож", "спальник", "котел", "спички", "еда", "карта"]
+backpack = []
+for i in things :
+    if len (i) <= 3:
+        backpack.append(i)
+print (f"В рюкзаке {len(backpack)} предметов: {backpack}")
